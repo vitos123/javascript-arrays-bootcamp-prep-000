849ec2158a1a80645eb9eb06f5d5fa897a4429e6
Victor Borov <vitos123@gmail.com>
2018-03-05 00:34:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,9 +6,4 @@ function addElementToBeginningOfArray() {
 function destructivelyAddElementToBeginningOfArray() {
 
 }
-function destructivelyAddElementToBeginningOfArray() {
-
-}
-function addElementToBeginningOfArray() {
-
-}
+]
