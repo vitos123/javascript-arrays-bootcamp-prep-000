680d06de91a970aa40bfb211a9e2c85b2d128af9
Victor Borov <vitos123@gmail.com>
2018-03-05 00:32:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray() {
+
+}
+function destructivelyAddElementToBeginningOfArray() {
+  
+}
